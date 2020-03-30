5c80e62ad97d5a9bcc4d654ff40b4454fff00d86
marcosvandrade <vascorj2006@gmail.com>
2020-03-30 12:56:17 -0300

    home30mar20

---
+++ server/src/test/java/server/springboot/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package server.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
